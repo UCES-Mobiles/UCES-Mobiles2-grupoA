93427fe7db9cb3a07509493780fa55a7393165ca
damian.zamora <damian.zamora@hotmail.com>
2025-10-03 19:37:22 -0300

    merge main and add viewByTeam

--- app/src/main/java/com/example/espncito/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/espncito/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.espncito.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.espncito.databinding.ActivityMainBinding
 import com.example.espncito.ui.viewByLeague.ViewByLeagueFragment
+import com.example.espncito.ui.viewByTeam.ViewByTeamActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupViewPager()
+
+        //Prueba para ir al activity de viewByTeam, eliminar luego de pruebas.
+/*        val intent = Intent(this, ViewByTeamActivity::class.java)
+        startActivity(intent)
+        finish()*/
     }
 
     private fun setupViewPager() {
